eee93273d3fe40c016c903466d2ea65a49470400
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-09-27 16:09:31 -0400

    feat: Add `DirectoryProcessor` utility for recursive file processing with YAML parsing support

---
+++ Bukkit/src/main/java/net/bitbylogic/utils/file/DirectoryProcessor.java
@@ -0,0 +1,79 @@
+package net.bitbylogic.utils.file;
+
+import lombok.NonNull;
+import net.bitbylogic.utils.config.ConfigParser;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectoryProcessor {
+
+    public static <T> List<T> processDirectory(@NonNull File folder, ConfigParser<T> parser) {
+        return processDirectory(folder, null, parser);
+    }
+
+    public static <T> List<T> processDirectory(@NonNull File folder, @Nullable String baseSection, ConfigParser<T> parser) {
+        if (!folder.isDirectory()) {
+            return new ArrayList<>();
+        }
+
+        return processDirectory(folder, baseSection, new ArrayList<>(), parser);
+    }
+
+    private static <T> List<T> processDirectory(@NonNull File directory, @Nullable String baseSection, @NonNull List<T> data, ConfigParser<T> parser) {
+        if (!directory.isDirectory()) {
+            return data;
+        }
+
+        File[] directoryFiles = directory.listFiles();
+
+        if (directoryFiles == null) {
+            return data;
+        }
+
+        for (File file : directoryFiles) {
+            if (file.isDirectory()) {
+                processDirectory(file, baseSection, data, parser);
+                continue;
+            }
+
+            processFile(file, baseSection, data, parser);
+        }
+
+        return data;
+    }
+
+    private static <T> void processFile(@NonNull File file, @Nullable String baseSection, @NonNull List<T> data, @NonNull ConfigParser<T> parser) {
+        if (!file.getName().endsWith(".yml")) {
+            return;
+        }
+
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        if(baseSection != null) {
+            ConfigurationSection section = config.getConfigurationSection(baseSection);
+
+            if(section == null) {
+                return;
+            }
+
+            section.getKeys(false).forEach(key -> {
+                ConfigurationSection keySection = section.getConfigurationSection(key);
+
+                if(keySection == null) {
+                    return;
+                }
+
+                parser.parseFrom(keySection).ifPresent(data::add);
+            });
+            return;
+        }
+
+        parser.parseFrom(config).ifPresent(data::add);
+    }
+
+}
