0c2b81b8f55632fbdeb8d5cbd8a7df838cb65378
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-09-28 08:41:56 -0400

    refactor: Rename `ConfigParser` to `ConfigSerializer` and update method names for clarity

---
+++ Bukkit/src/main/java/net/bitbylogic/utils/config/ConfigSerializer.java
@@ -0,0 +1,14 @@
+package net.bitbylogic.utils.config;
+
+import lombok.NonNull;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Optional;
+
+public interface ConfigSerializer<O> {
+
+    Optional<O> serializeFrom(@NonNull ConfigurationSection section);
+
+    ConfigurationSection serializeTo(@NonNull ConfigurationSection section, @NonNull O object);
+
+}
