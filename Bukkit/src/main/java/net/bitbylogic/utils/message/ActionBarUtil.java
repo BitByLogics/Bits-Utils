8d8a3d35ce6a0d02c182864bfe693366abe8e937
Justin <33465177+BitByLogics@users.noreply.github.com>
2026-02-10 07:25:11 -0500

    feat: add `sendActionBar` method with TextComponent support to enhance message flexibility

--- Bukkit/src/main/java/net/bitbylogic/utils/message/ActionBarUtil.java
+++ Bukkit/src/main/java/net/bitbylogic/utils/message/ActionBarUtil.java
@@ -3,6 +3,7 @@ package net.bitbylogic.utils.message;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.NonNull;
+import net.kyori.adventure.text.TextComponent;
 import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.entity.Player;
 
@@ -12,6 +13,11 @@ import java.util.concurrent.TimeUnit;
 public class ActionBarUtil {
 
     private static final Cache<UUID, String> LAST_ACTION_BAR = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS).build();
+
+    public static void sendActionBar(@NonNull Player player, @NonNull String id, @NonNull TextComponent message) {
+        MessageUtil.send(player, message);
+        LAST_ACTION_BAR.put(player.getUniqueId(), id);
+    }
 
     public static void sendActionBar(@NonNull Player player, @NonNull String id, @NonNull String message) {
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, MessageUtil.praiseMD5(MessageUtil.deserialize(message)));
