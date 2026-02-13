c3261ab8c1d73caec8d1c97f2727089591f8cb23
Justin <33465177+BitByLogics@users.noreply.github.com>
2026-02-13 07:56:18 -0500

    fix: enforce non-italic styling for ItemMeta display name and lore

--- Paper/src/main/java/net/bitbylogic/utils/item/PaperItemStackUtil.java
+++ Paper/src/main/java/net/bitbylogic/utils/item/PaperItemStackUtil.java
@@ -1,6 +1,7 @@
 package net.bitbylogic.utils.item;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,7 +11,7 @@ import java.util.function.Function;
 public class PaperItemStackUtil {
 
     public static void setName(@NotNull ItemMeta meta, @NotNull Component component) {
-        meta.displayName(component);
+        meta.displayName(component.style(style -> style.decoration(TextDecoration.ITALIC, false)));
     }
 
     public static void updateName(@NotNull ItemMeta meta, @NotNull Function<Component, Component> componentUpdater) {
@@ -18,7 +19,7 @@ public class PaperItemStackUtil {
             return;
         }
 
-        meta.displayName(componentUpdater.apply(meta.displayName()));
+        meta.displayName(componentUpdater.apply(meta.displayName()).style(style -> style.decoration(TextDecoration.ITALIC, false)));
     }
 
     public static void updateLore(@NotNull ItemMeta meta, @NotNull Function<List<Component>, List<Component>> componentUpdater) {
@@ -26,10 +27,15 @@ public class PaperItemStackUtil {
             return;
         }
 
-        meta.lore(componentUpdater.apply(meta.lore()));
+        List<Component> lore = componentUpdater.apply(meta.lore());
+        lore.replaceAll(component -> component.style(style -> style.decoration(TextDecoration.ITALIC, false)));
+
+        meta.lore(lore);
     }
 
     public static void setLore(@NotNull ItemMeta meta, @NotNull List<Component> lore) {
+        lore.replaceAll(component -> component.style(style -> style.decoration(TextDecoration.ITALIC, false)));
+
         meta.lore(lore);
     }
 
